b1e6802819785e1e5b610eb62170a3b2b0358fcc
xuwnagjing <xuwangjing3419@gmail.com>
2023-10-01 12:31:13 +0800

    Create 2091 sizeyunsuan.c

---
+++ chenxushejijichu (2023-2024)/Text 2/2091 sizeyunsuan.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{	
+	int a, b;
+	
+	scanf("%d %d\n", &a, &b);
+	printf("%d+%d=%d\n", a, b, a+b);
+	printf("%d-%d=%d\n", a, b, a-b);
+	printf("%d*%d=%d\n", a, b, a*b);
+	printf("%d/%d=%d\n", a, b, a/b);
+	
+	return 0;
+ } 
