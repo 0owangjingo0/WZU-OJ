ce7afd07e83aebc9c93d56f327a86cbfb00172a4
xuwnagjing <xuwangjing3419@gmail.com>
2023-10-01 12:27:08 +0800

    Create 0180 yibuyigejiaoyin a plus b.c

---
+++ chenxushejijichu (2023-2024)/Basic exercise/0180 yibuyigejiaoyin a plus b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    while (n-- >= 1)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+}
