1a83fb0dca2d44fdae5004099e5397339370f6ab
xuwnagjing <xuwangjing3419@gmail.com>
2023-10-01 12:31:45 +0800

    Create 2101 runnian.c

---
+++ chenxushejijichu (2023-2024)/Text 4/2101 runnian.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{   
+    int year;
+    int flag = 0;
+
+    scanf("%d", &year);
+
+    if (
+        (year % 4 == 0)
+        &&
+        (year % 100 != 0)
+    )
+    {
+        flag = 1;
+    }
+    else if (year % 400 == 0)
+    {
+        flag = 1;
+    }
+
+    flag ? printf("yes\n") : printf("no\n");
+
+    return 0;
+}
