e2eed48dfbf04f58e46a605fe0153591a12d96fd
xuwnagjing <xuwangjing3419@gmail.com>
2023-10-01 12:36:32 +0800

    Create 2100 zifujiaohuan.c

---
+++ chenxushejijichu (2023-2024)/Work 2/2100 zifujiaohuan.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{	
+	char a, b;
+	
+	a = getchar();
+	b = getchar(); 
+	b = getchar();
+	
+	putchar(b);
+	printf(" ");
+	putchar(a);
+	printf("\n");
+	
+	return 0;
+ } 
