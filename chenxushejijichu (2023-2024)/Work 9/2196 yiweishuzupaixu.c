aa54c7e566435d6b3cc2b33a96bf24d2771bb977
xuwnagjing <xuwangjing3419@gmail.com>
2023-10-01 12:38:44 +0800

    Create 2196 yiweishuzupaixu.c

---
+++ chenxushejijichu (2023-2024)/Work 9/2196 yiweishuzupaixu.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void charupaixu(int lst[], int p);
+
+int main(void)
+{
+    int m;
+    int lst[100];
+    int p = 0;
+    scanf("%d", &m);
+        
+    while(m >= 1)
+    {
+        scanf("%d", &lst[p]);
+        p++;
+
+        m--;
+    }
+    charupaixu(lst, p);
+        
+    int i;
+    for (i=0; i<p-1; i++)
+    {
+        printf("%d ", lst[i]);
+    }
+    printf("%d \n", lst[i]);
+
+
+    return 0;
+}
+
+void charupaixu(int lst[], int p)
+{
+    int t;
+
+    for (int i=1; i <= p-1; i++)
+    {
+        t = lst[i];
+        int flag = 0;
+        
+        int j = i - 1;
+        while (j >= 0)
+        {
+            if (t < lst[j])
+            {
+                lst[j+1] = lst[j];
+            }
+            else
+            {
+                break;
+            }
+
+            j--;
+        }
+        lst[j+1] = t;
+    }
+}
