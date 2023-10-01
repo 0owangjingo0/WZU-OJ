a4cc55f9d01c0f1cb8dbd79cebfe44733bc3e979
xuwnagjing <xuwangjing3419@gmail.com>
2023-10-01 12:27:28 +0800

    Create 0199 qiu PI dezhi.c

---
+++ chenxushejijichu (2023-2024)/Basic exercise/0199 qiu PI dezhi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	double s = 1;
+	for(i=1; i <= 100; i++)
+	{
+		s *= (i*2.0)/(i*2-1);
+		s *= (i*2.0)/(i*2+1);
+	}
+	printf("%.3f\n", s*2);
+	return 0;
+}
