d03df68d9c7ef753e739a0f7c5f9f6ec5e3255f2
xuwnagjing <xuwangjing3419@163.com>
2023-10-31 21:21:59 +0800

    Create 1294 xueshengchengjichuli (2).c

---
+++ chenxushejijichu (2023-2024)/Work 13/1294 xueshengchengjichuli (2).c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char name[100][20];
+    double score[100][3];
+    double sum[100];
+    int p = 0;
+
+    while (
+        scanf(
+            "%s %lf %lf %lf",
+            name[p],
+            &score[p][0],
+            &score[p][1],
+            &score[p][2]
+            ) != EOF
+    )
+    {
+        sum[p] = score[p][0] + score[p][1] + score[p][2];
+        p++;
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < p - 1 - i; j++)
+        {
+            if (sum[j] < sum[j + 1])
+            {
+                double temp = sum[j];
+                sum[j] = sum[j + 1];
+                sum[j + 1] = temp;
+
+                char temp_name[20];
+                strcpy(temp_name, name[j]);
+                strcpy(name[j], name[j + 1]);
+                strcpy(name[j + 1], temp_name);
+
+                double temp_score[3];
+                for (int k = 0; k < 3; k++)
+                {
+                    temp_score[k] = score[j][k];
+                    score[j][k] = score[j + 1][k];
+                    score[j + 1][k] = temp_score[k];
+                }
+            }   
+        }
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+        printf(
+            "%s %.1lf %.1lf %.1lf %.1lf\n",
+            name[i],
+            score[i][0],
+            score[i][1],
+            score[i][2],
+            sum[i]
+            );
+    }
+
+    return 0;
+}
