c66210854d6f37b8a42ece85a5551686d03d3bd5
xuwnagjing <xuwangjing3419@163.com>
2024-03-09 14:56:25 +0800

    Create 3015 changdu.cpp

---
+++ suanfayushujujiegou (2023-2024)/Test 2/3015 changdu.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+typedef struct Node{
+	ElementType data;
+	struct Node  *next;
+}Node, *LinkList;
+
+int getLength(LinkList L);
+
+int main()
+{
+    /*此处代码由测试程序自动添加，主要为了向顺序表中插入数据
+	并输出数据,你无需关心此处代码的具体实现细节。
+	如果有必要，请自己添加代码以测试你的函数是否正确。 
+    */
+    return 0;
+}
+/*你的提交的代码将被添加在此处，请完成题目所要求的函数的定义*/
+int getLength(LinkList L)
+{
+    int cont = 0;
+    LinkList p = L -> next;
+    
+    while (p != NULL)
+    {
+        cont++;
+        p = p -> next;
+    }
+
+    return cont;
+}
