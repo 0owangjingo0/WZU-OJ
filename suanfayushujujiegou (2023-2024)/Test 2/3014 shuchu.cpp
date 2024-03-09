04198c7f38ef103c136778c683f72c35181a3e83
xuwnagjing <xuwangjing3419@163.com>
2024-03-09 14:56:23 +0800

    Create 3014 shuchu.cpp

---
+++ suanfayushujujiegou (2023-2024)/Test 2/3014 shuchu.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+typedef struct Node{
+	ElementType data;
+	struct Node  *next;
+}Node, *LinkList;
+
+void printList(LinkList L);
+
+int main()
+{
+    /*此处代码由测试程序自动添加，主要为了向顺序表中插入数据
+	并输出数据,你无需关心此处代码的具体实现细节。
+	如果有必要，请自己添加代码以测试你的函数是否正确。 
+    */
+    return 0;
+}
+/*你的提交的代码将被添加在此处，请完成题目所要求的函数的定义*/
+void printList(LinkList L)
+{
+    LinkList p = L -> next;
+    while (p != NULL)
+    {
+        printf("%d ", p -> data);
+        p = p -> next;
+    }
+    putchar('\n');
+}
